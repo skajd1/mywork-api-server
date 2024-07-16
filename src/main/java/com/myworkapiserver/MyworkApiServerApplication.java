dc51e39bd2ee78f181604726eafd8f164a19f39a
skajd1 <woochul102@gmail.com>
2024-07-16 21:48:30 +0900

    feat: first commit

---
+++ src/main/java/com/myworkapiserver/MyworkApiServerApplication.java
@@ -0,0 +1,13 @@
+package com.myworkapiserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyworkApiServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyworkApiServerApplication.class, args);
+    }
+
+}
