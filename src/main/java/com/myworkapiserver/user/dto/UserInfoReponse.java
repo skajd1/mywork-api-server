edb162fb40568d5a250edb33b4792584deb60498
skajd1 <woochul102@gmail.com>
2024-07-16 22:17:24 +0900

    feat: added UserDto

---
+++ src/main/java/com/myworkapiserver/user/dto/UserInfoReponse.java
@@ -0,0 +1,10 @@
+package com.myworkapiserver.user.dto;
+
+
+import lombok.Data;
+
+@Data
+public class UserInfoReponse {
+    private int uid;
+    private String name;
+}
---
+++ src/main/java/com/myworkapiserver/user/dto/UserInfoRequest.java
@@ -0,0 +1,8 @@
+package com.myworkapiserver.user.dto;
+
+import lombok.Data;
+
+@Data
+public class UserInfoRequest {
+    private String name;
+}
