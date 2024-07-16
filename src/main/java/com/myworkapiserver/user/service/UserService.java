ff1bd7358655cb459d1daddaf935a5ffb1916ca2
skajd1 <woochul102@gmail.com>
2024-07-16 22:19:18 +0900

    feat: added UserService

---
+++ src/main/java/com/myworkapiserver/user/service/UserService.java
@@ -0,0 +1,9 @@
+package com.myworkapiserver.user.service;
+
+public interface UserService {
+    void getUserList();
+    void getUser();
+    void createUser();
+    void updateUser();
+    void deleteUser();
+}
---
+++ src/main/java/com/myworkapiserver/user/service/UserServiceImpl.java
@@ -0,0 +1,31 @@
+package com.myworkapiserver.user.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserServiceImpl implements UserService{
+    @Override
+    public void getUserList() {
+
+    }
+
+    @Override
+    public void getUser() {
+
+    }
+
+    @Override
+    public void createUser() {
+
+    }
+
+    @Override
+    public void updateUser() {
+
+    }
+
+    @Override
+    public void deleteUser() {
+
+    }
+}
