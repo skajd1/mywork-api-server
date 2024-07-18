26bbebd05c6cff9d77620ce8da34600a5d0ef881
skajd1 <woochul102@gmail.com>
2024-07-18 23:23:20 +0900

    fix: 기본생성자 추가
    RequestBody로 받을 때 이거 없으면 안받아짐

--- src/main/java/com/myworkapiserver/user/dto/UserInfoRequest.java
+++ src/main/java/com/myworkapiserver/user/dto/UserInfoRequest.java
@@ -2,9 +2,11 @@ package com.myworkapiserver.user.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class UserInfoRequest {
     private String name;
 }
